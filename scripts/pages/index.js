5e192873ab6ecf5851df44d6c9fd623acaf9b4d8
maiinawa <ninamaite1@gmail.com>
2022-01-29 03:22:43 +0100

    fetch photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,10 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const photographers = await fetch('/data/photographers.json')
+        .then(response => response.json())
+        .then(data => data)
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return photographers
     }
 
     async function displayData(photographers) {
@@ -39,6 +21,8 @@
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+        console.log(photographers);
+
     };
     
     init();
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "production",
   entry: {
     polyfill: "babel-polyfill",
-    app: "./src/index.js"
+    app: "./scripts/pages/index.js"
   },
   output: {
     filename: "[name].bundle.js",
